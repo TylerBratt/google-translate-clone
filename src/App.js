23e66c97579355508a8e2d5a7a5d7696747230d7
Tyler Bratt <tyler~bratt@hotmail.com>
2022-04-22 17:28:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -13,23 +13,8 @@ const App = () => {
   const [translatedText, setTranslatedText] = useState("")
 
   const getLanguages = async () => {
-
-
-const options = {
-  method: 'GET',
-  url: 'https://google-translate20.p.rapidapi.com/languages',
-  headers: {
-    'X-RapidAPI-Host': 'google-translate20.p.rapidapi.com',
-    'X-RapidAPI-Key': '6784f1e9afmsh837955dce65ecb1p18fdf8jsn21fcd3082e8f'
-  }
-};
-
-axios.request(options).then(function (response) {
-  const arrayOfData = Object.keys(response.data.data).map(key => response.data.data[key])
-  setLanguages(arrayOfData)
-}).catch(function (error) {
-	console.error(error);
-});
+    const response = await axios('http://localhost:8000/languages')
+    setLanguages(response.data)
   }
 
   const handleClick = () => {
@@ -37,30 +22,15 @@ axios.request(options).then(function (response) {
     setOutputLanguage(inputLanguage);
   }
 
-  const translate = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://google-translate20.p.rapidapi.com/translate',
-      params: {
-        text: textToTranslate,
-        tl: outputLanguage,
-        sl: inputLanguage
-      },
-      headers: {
-        'X-RapidAPI-Host': 'google-translate20.p.rapidapi.com',
-        'X-RapidAPI-Key': '6784f1e9afmsh837955dce65ecb1p18fdf8jsn21fcd3082e8f'
-      }
-    };
-    
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-      setTranslatedText(response.data.data.translation)
-    }).catch(function (error) {
-      console.error(error);
-    });
+  const translate = async () => {
+    const data = {
+      textToTranslate, outputLanguage, inputLanguage
+    }
+    const response = await axios('http://localhost:8000/translation', {
+      params: data
+    })
+    setTranslatedText(response.data)
   }
-
-  console.log("translated", translatedText)
 
   useEffect(() => {
     getLanguages()
